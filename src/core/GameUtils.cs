ce6f45f947111fdc6b9ce2e681e45b9d37921068
Devwarlt <devwarlt@loesoft.org>
2019-11-13 13:26:07 -0200

    Form deprecated + new functionality to change window name

---
+++ src/core/GameUtils.cs
@@ -0,0 +1,15 @@
+﻿using org.loesoftgames.rotmg.rultra;
+
+namespace org.loesoft.rotmg.ultra.core
+{
+    public static class GameUtils
+    {
+        public static void UpdateCaption(string pattern = null) =>
+            App.window.Caption =
+                string.Format("{0} - version: {1}{2}",
+                    App.name,
+                    App.version,
+                    string.IsNullOrEmpty(pattern) ? "" : $" | {pattern}"
+                );
+    }
+}
